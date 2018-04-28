c1ab7259b9b6b12163b5a7197768e1f8147922ff
jw <cjw0515@naver.com>
2018-04-29 04:20:12 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
 import InputTextContainer from "./components/InputText";
 import TextList from "./components/TextList";
-//import { TextProvider } from "./contexts/TextContext";
 import { TextProvider } from "./contexts/addContext";
+
+const AppProvider = ({ contexts, children }) => contexts.reduce(
+  (prev, context) => React.createElement(context, {
+    children: prev
+  }), 
+  children
+);
 
 class App extends Component {
   render() {
     return (
-      <TextProvider>
+      <AppProvider
+        contexts={[TextProvider]}
+      >               
         <div className="container">
           <InputTextContainer />
           <TextList />
         </div>
-      </TextProvider>
+      </AppProvider>
     );
   }
 }
